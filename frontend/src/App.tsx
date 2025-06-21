3bc5b2bb345cc60663ddded9d2b14b8259edd026
Sergiu Gherasim <sergiugherasim@Sergius-MacBook-Pro.local>
2025-06-21 16:40:26 +0300

    feat: add react router dom

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,12 @@
-import { Button } from './components/ui/button'
-import { Input } from './components/ui/input'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Root from './routes/root'
 
-const App = () => {
+export default function App() {
   return (
-    <div className="min-w-screen min-h-screen flex justify-center items-center bg-neutral-200">
-      <div className="w-[720px]">
-        <Input placeholder="Enter YouTube video URL..." className="h-10 bg-white" />
-        <Button className="mt-2">Analyze Video</Button>
-      </div>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Root />}></Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
-
-export default App
