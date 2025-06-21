a8dec12e83e2629c9eae5fb9ac573cb63343a0d4
Sergiu Gherasim <sergiugherasim@Sergius-MacBook-Pro.local>
2025-06-21 16:16:43 +0300

    feat: add trascript route & change backend to from commonjs to es6

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,15 @@
+import { useEffect, useState } from 'react'
 import { Button } from './components/ui/button'
+import { YoutubeTranscript } from 'youtube-transcript'
 
 const App = () => {
-  return (
-    <div>
-      <Button>Hello</Button>
-    </div>
-  )
+  const [data, setData] = useState('')
+
+  useEffect(() => {
+    YoutubeTranscript.fetchTranscript('').then((d: any) => setData(d))
+  }, [])
+
+  return <div>{data}</div>
 }
 
 export default App
