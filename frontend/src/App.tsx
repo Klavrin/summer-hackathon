f7b235428072f7ec6bea3c758aceb3d4a7234c2b
Sergiu Gherasim <sergiugherasim@Sergius-MacBook-Pro.local>
2025-06-21 12:48:30 +0300

    feat: add shadcn

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,11 @@
+import { Button } from './components/ui/button'
+
 const App = () => {
-  return <div>App</div>
+  return (
+    <div>
+      <Button>Hello</Button>
+    </div>
+  )
 }
 
 export default App
