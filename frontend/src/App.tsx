4179081fe55c9c8f1039f63855b1347c36bc9c21
Sergiu Gherasim <sergiugherasim@Sergius-MacBook-Pro.local>
2025-06-21 16:33:07 +0300

    feat: add button

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,9 +3,10 @@ import { Input } from './components/ui/input'
 
 const App = () => {
   return (
-    <div className="min-w-screen min-h-screen flex justify-center items-center">
+    <div className="min-w-screen min-h-screen flex justify-center items-center bg-neutral-200">
       <div className="w-[720px]">
-        <Input placeholder="Enter YouTube video URL..." className="h-10" />
+        <Input placeholder="Enter YouTube video URL..." className="h-10 bg-white" />
+        <Button className="mt-2">Analyze Video</Button>
       </div>
     </div>
   )
