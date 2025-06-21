8f3cb341643408f43733443660c3ef2bbc129c92
kynexi <184446386+kynexi@users.noreply.github.com>
2025-06-21 17:48:20 +0300

    Fix transcript auto language problem

--- backend-python/main.py
+++ backend-python/main.py
@@ -7,9 +7,10 @@ def extract_video_id(link):
 
 def get_transcript(link):
     id = extract_video_id(link)
-    transcript = YouTubeTranscriptApi.get_transcript(id)
+    transcript = YouTubeTranscriptApi.get_transcript(id, languages=['en-US'])
     text_only = [entry['text'] for entry in transcript]
     full_text = ' '.join(text_only)
     print(full_text)
 
 '''Execute get_trancript(link), receive long ass string with transcript'''
+get_transcript('https://www.youtube.com/watch?v=Lxa8poQYRtc')
