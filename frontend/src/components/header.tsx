518ea1bec3c9eb8fd5a2276da0f2d87aa98567b6
Sergiu Gherasim <sergiugherasim@Sergius-MacBook-Pro.local>
2025-06-21 17:08:25 +0300

    feat: add select component

--- frontend/src/components/header.tsx
+++ frontend/src/components/header.tsx
@@ -1,8 +1,26 @@
 import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar'
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue
+} from './ui/select'
 
 const Header = () => {
   return (
     <header className="bg-neutral-200 flex justify-end items-center p-2">
+      <Select>
+        <SelectTrigger className="w-[180px]">
+          <SelectValue placeholder="Theme" />
+        </SelectTrigger>
+        <SelectContent>
+          <SelectItem value="light">Light</SelectItem>
+          <SelectItem value="dark">Dark</SelectItem>
+          <SelectItem value="system">System</SelectItem>
+        </SelectContent>
+      </Select>
+
       <Avatar className="w-[35px] h-[35px] hover:outline-4 cursor-pointer">
         <AvatarImage src="https://github.com/shadcn.png" />
         <AvatarFallback>Brinza Vasile</AvatarFallback>
